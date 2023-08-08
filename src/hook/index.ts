d1dc99ab865a8b38ae32dfcba8186147c5600847
Cecil Tantay <cjbtantay@gmail.com>
2023-08-08 02:02:18 -0700

    reorganize files and folder for simplicity

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,2 +1,17 @@
-export * from "./hook-handler";
-export * from "./schema";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import errorLogger from "@middy/error-logger";
+import httpSecurityHeaders from "@middy/http-security-headers";
+
+import { validateRequestBody } from "./schema";
+import { parseJsonBody } from "../shared/utilities/middlewares";
+import { surveyHookHandler } from "./hook-handler";
+
+export const handler = middy(surveyHookHandler)
+  .before(parseJsonBody())
+  .before(validateRequestBody())
+  .use(httpEventNormalizer())
+  .use(errorLogger())
+  .use(httpErrorHandler())
+  .use(httpSecurityHeaders());
--- src/responses/index.ts
+++ src/responses/index.ts
@@ -1,2 +1,14 @@
-export * from "./responses-handler";
-export * from "./schema";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import errorLogger from "@middy/error-logger";
+import httpSecurityHeaders from "@middy/http-security-headers";
+import { surveyResponseHandler } from "./responses-handler";
+import { validateRequestBody } from "./schema";
+
+export const handler = middy(surveyResponseHandler)
+  .before(validateRequestBody())
+  .use(httpEventNormalizer())
+  .use(errorLogger())
+  .use(httpErrorHandler())
+  .use(httpSecurityHeaders());
