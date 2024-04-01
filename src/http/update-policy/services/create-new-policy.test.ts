7a91117403060b106f07e0a6c8963ea3389453fd
Cecil J. Tantay <me@cjtantay.com>
2024-04-01 03:49:33 -0700

    more updates

--- src/http/update-policy/services/create-new-policy.test.ts
+++ src/http/update-policy/services/create-new-policy.test.ts
@@ -1,4 +1,4 @@
-import { db } from '../../../../data/knex';
+import { db } from '../../../../data';
 import { AiPolicy } from '../../../shared';
 import { createNewPolicy } from './'; // Update the path appropriately
 import mockKnex from 'mock-knex';
@@ -28,6 +28,47 @@ describe('createNewPolicy', () => {
           title: 'Section heading',
         },
       ],
+      metadata: {
+        organization: null,
+      },
+    } as AiPolicy;
+
+    tracker.on('query', (query) => {
+      expect(query.method).toBe('insert');
+      query.response([mockId]);
+    });
+
+    const [returnedId] = await createNewPolicy(mockId, mockPolicy);
+
+    expect(returnedId).toBe(mockId);
+  });
+});
+
+describe('HGSE createNewPolicy', () => {
+  beforeAll(() => {
+    mockKnex.mock(db);
+    tracker.install();
+  });
+
+  afterAll(() => {
+    tracker.uninstall();
+    mockKnex.unmock(db);
+  });
+
+  it('should insert a new policy and return its ID', async () => {
+    const mockId = '1234';
+    const mockPolicy = {
+      id: 'Generated ULID',
+      heading: 'Course information like title, number, instructor, etc.',
+      sections: [
+        {
+          id: 'Generated ULID',
+          title: 'Section heading',
+        },
+      ],
+      metadata: {
+        organization: 'harvard',
+      },
     } as AiPolicy;
 
     tracker.on('query', (query) => {
